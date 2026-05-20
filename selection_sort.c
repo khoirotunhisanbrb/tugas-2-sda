974bcd6c3a3ed91e488d5227ff7f1bcce02ea418
nawalazqia <nawalazqiaa26@gmail.com>
2026-05-20 22:39:40 +0700

    Create selection_sort.c

---
+++ selection_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+void selection_sort_int(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        if (min_idx != i) {
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+}
+
+void selection_sort_string(char *arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (strcmp(arr[j], arr[min_idx]) < 0) {
+                min_idx = j;
+            }
+        }
+        if (min_idx != i) {
+            char *temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+}
