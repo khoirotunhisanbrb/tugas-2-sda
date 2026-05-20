7dbfece7ae7a45280bf9386558d12178e21c3958
Syella25 <SYELLA@mhs.usk.ac.id>
2026-05-20 22:13:21 +0700

    Create insertion_sort.c

---
+++ insertion_sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void insertion_sort_int(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void insertion_sort_string(char *arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        char *key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && strcmp(arr[j], key) > 0) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
