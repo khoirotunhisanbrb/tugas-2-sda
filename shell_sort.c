760bdf6f288fead0f9f64bb5a0f5f4f754907185
nawalazqia <nawalazqiaa26@gmail.com>
2026-05-25 15:48:31 +0700

    Update shell_sort.c

--- shell_sort.c
+++ shell_sort.c
@@ -1,28 +1,40 @@
+```c
 #include <stdio.h>
 #include <string.h>
 
 void shell_sort_int(int arr[], int n) {
-    for (int gap = n / 2; gap > 0; gap /= 2) {
-        for (int i = gap; i < n; i++) {
-            int temp = arr[i];
-            int j;
+    int gap, i, j, temp;
+
+    for (gap = n / 2; gap > 0; gap /= 2) {
+        for (i = gap; i < n; i++) {
+            temp = arr[i];
+
             for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
                 arr[j] = arr[j - gap];
             }
+
             arr[j] = temp;
         }
     }
 }
 
 void shell_sort_string(char *arr[], int n) {
-    for (int gap = n / 2; gap > 0; gap /= 2) {
-        for (int i = gap; i < n; i++) {
-            char *temp = arr[i];
-            int j;
-            for (j = i; j >= gap && strcmp(arr[j - gap], temp) > 0; j -= gap) {
+    int gap, i, j;
+    char *temp;
+
+    for (gap = n /2; gap > 0; gap /= 2) {
+        for (i = gap; i < n; i++) {
+            temp = arr[i];
+
+            for (j = i; j >= gap &&
+                 strcmp(arr[j - gap], temp) > 0; j -= gap) {
+
                 arr[j] = arr[j - gap];
             }
+
             arr[j] = temp;
         }
     }
 }
+```
+
